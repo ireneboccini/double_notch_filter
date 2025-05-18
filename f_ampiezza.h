b895b48bea182469e7a5424d330a6e0186194766
ireneboccini <ireneboccini@gmail.com>
2025-05-18 03:13:02 +0200

    Update f_ampiezza.h
    da variabili fisse a libere

--- f_ampiezza.h
+++ f_ampiezza.h
@@ -9,17 +9,15 @@ double f_ampiezza(double *x, double *par) {
     double f = x[0];
     double w = 2 * M_PI * f;
 
-    // Parametri fissi (metti i valori reali!)
-    double C1 =  101.2 * 1e-9;
-    double C2 = 22 * 1e-9 ;
-    double L1 = 10 * 1e-3 ;
-    double L2 = 4.7 * 1e-3;
-    double RL1 = 42;
-    double RL2 = 78;
-
-    // Parametro libero: R
-    double R = par[0];
-
+   double A = par[0];
+    double R = par[1];
+    double C1 = par[2];
+     double C2 = par[3];
+      double L1 = par[4];
+      double L2 = par [5];
+      double RL1 = par [6];
+      double RL2 = par[7];
+    
     double x_re = R * (1 - w*w*L2*C2 - w*w*L1*C1 + pow(w,4)*L1*C1*L2*C2 - w*w*C1*C2*RL1*RL2);
     double x_im = R * (w*C2*RL2 + w*C1*RL1 - pow(w,3)*L1*C1*C2*RL2 - pow(w,3)*L2*C2*C1*RL1);
     double z_re = -w*w*L1*C2*RL2 + RL1 - w*w*L2*C2*RL1 - w*w*L2*C1*RL1 + RL2 - w*w*L1*C1*RL2 + x_re;
@@ -29,7 +27,7 @@ double f_ampiezza(double *x, double *par) {
     double num_im = x_im * z_re - x_re * z_im;
     double den = z_re*z_re + z_im*z_im;
 
-    return ( 2.5 * sqrt((num_re*num_re + num_im*num_im) / (den*den))) ; // 2.5 voltaggio, solo un picco ( 5V era il voltaggio picco-picco)
+    return ( A * sqrt((num_re*num_re + num_im*num_im) / (den*den))) ; // 2.5 voltaggio, solo un picco ( 5V era il voltaggio picco-picco)
 }
 
 #endif
